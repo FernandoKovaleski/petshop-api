9c3a443430651158e0a730d3ab5af0ebb53da899
fernando kovaleski <fernandokovaleski@hotmail.com>
2021-12-16 20:43:56 -0300

    javaruntime

--- src/main/java/br/com/tt/petshop/client/credito/CreditoApiClient.java
+++ src/main/java/br/com/tt/petshop/client/credito/CreditoApiClient.java
@@ -4,8 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 @FeignClient(name = "credito-api",
-            url = "https://imersao-credito-api.herokuapp.com")
+            url = "${client.credito.url}in")
 public interface CreditoApiClient {
 
     @GetMapping("/credito/{cpf}")
